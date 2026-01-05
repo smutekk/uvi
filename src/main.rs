dd4d6878e350ba198c6043b77d92a755a4e48e03
kketums <ssmutekk@proton.me>
2026-01-04 21:38:37 -0500

    Added meson support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use fstrings::*;
 use git2::Repository;
 use reqwest::blocking;
 use std::env;
@@ -8,6 +9,8 @@ use std::path::Path;
 use std::path::PathBuf;
 use tar::Archive;
 use xz2::read::XzDecoder;
+
+mod compilers;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -22,7 +25,7 @@ struct Args {
     user: bool,
 
     #[arg(long, default_value = "/usr")]
-    destination: String,
+    prefix: String,
 
     #[arg(long)]
     fast: bool, // essentially --noconfirm
@@ -32,19 +35,21 @@ struct Args {
 
     #[arg(long)]
     systemd: bool,
+
+    #[arg(long, default_value = "none")]
+    repo: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    let mut target_destination = Path::new(&args.destination);
+    let mut target_destination = Path::new(&args.prefix);
     let filename = args
         .name
         .as_str()
         .split('/')
         .last()
         .unwrap_or("download.tmp");
-
     let home_path = match env::var("HOME") {
         Ok(p) => PathBuf::from(p),
         Err(_) => {
@@ -59,16 +64,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if args.user {
         target_destination = Path::new(&home_path); //get username
     }
-
-    println!("Home dir is: {:?}", home_path);
-    println!("Prefix is: {}", target_destination.display());
-
     if args.link {
         println!("Using url: {}", args.name.as_str());
         if args.name.as_str().ends_with(".git") {
             git_repo(args.name.as_str(), Path::new(&file_path))?;
         } else {
-            download(args.name.as_str(), Path::new(&file_path), Path::new(&cache))?;
+            download(
+                args.name.as_str(),
+                Path::new(&file_path),
+                Path::new(&cache),
+                target_destination,
+            )?;
         }
         // soo easy
     } else {
@@ -76,10 +82,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // bit harder
     }
 
+    println!("Home dir is: {:?}", home_path);
+    println!("Prefix is: {}", target_destination.display());
+
     Ok(())
 }
 
-fn download(url: &str, destination: &Path, cache: &Path) -> Result<(), Box<dyn std::error::Error>> {
+fn download(
+    url: &str,
+    destination: &Path,
+    cache: &Path,
+    prefix: &Path,
+) -> Result<(), Box<dyn std::error::Error>> {
     let response = blocking::get(url)?;
     let mut dest = File::create(destination)?;
     let content = response.bytes()?;
@@ -87,7 +101,7 @@ fn download(url: &str, destination: &Path, cache: &Path) -> Result<(), Box<dyn s
     copy(&mut content.as_ref(), &mut dest)?;
     drop(dest);
 
-    unpack(Path::new(destination), &cache);
+    unpack(Path::new(destination), &cache, prefix)?;
 
     Ok(())
 }
@@ -103,26 +117,56 @@ fn git_repo(url: &str, destination: &Path) -> Result<(), Box<dyn std::error::Err
     Ok(())
 }
 
-fn unpack(file_to_unpack: &Path, destination: &Path) {
+fn unpack(
+    file_to_unpack: &Path,
+    destination: &Path,
+    prefix: &Path,
+) -> Result<PathBuf, Box<dyn std::error::Error>> {
     if file_to_unpack.extension().map_or(false, |ext| ext == "xz") {
         println!("XZ file detected! starting unpack process..");
 
-        let file = match File::open(file_to_unpack) {
-            Ok(f) => f,
-            Err(e) => {
-                eprintln!("Failed to open file: {}", e);
-                return;
-            }
-        };
+        let file = File::open(file_to_unpack)?;
 
         let decompressor = XzDecoder::new(file);
         let mut archive = Archive::new(decompressor);
 
-        archive.unpack(destination).expect("Failed");
+        archive.unpack(destination)?;
 
-        if destination1
+        let unpacked_file = file_to_unpack
+            .file_stem()
+            .and_then(|s| Path::new(s).file_stem())
+            .and_then(|s| s.to_str())
+            .unwrap_or("default");
+
+        install(&destination.join(unpacked_file.to_string()), prefix);
+        return Ok(destination.to_path_buf());
+    }
+
+    Err("not an xz".into())
+}
+
+fn install(destination: &Path, install_dir: &Path) {
+    let dest_str = destination.to_str();
+
+    println!("unpacked file located at: {}", dest_str.unwrap());
+
+    if destination.join("meson.build").exists() {
+        println!("Building with meson..");
+
+        let build_dir = destination.join("build");
+
+        println!("Build directory is: {}", build_dir.to_string_lossy());
+
+        compilers::meson::build(
+            destination.to_str().unwrap(),
+            &build_dir.to_string_lossy(),
+            "--prefix=/usr", // not instal_dir/prefix yet
+        );
+    } else {
+        eprintln!("No meson.build found..?");
     }
 }
+
 // // fn deb() {}
 
 // // fn rpm() {}
