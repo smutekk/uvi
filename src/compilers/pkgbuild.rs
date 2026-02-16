5157a7d3d5acf1e6e0614669720917d41cef1b8e
kketums <ssmutekk@proton.me>
2026-02-16 16:41:20 -0500

    Added a 'parse' function that allows for easy extraction of variables in a PKGBUILD

--- src/compilers/pkgbuild.rs
+++ src/compilers/pkgbuild.rs
@@ -1,4 +1,6 @@
-use regex::Regex;
+use regex::{Captures, Regex};
+use std::ascii::AsciiExt;
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 use std::process::{Command, Stdio};
@@ -10,7 +12,27 @@ pub fn build(src_dir: &Path, cache: &str) {
     make(&pkgbuild_path);
 }
 
+fn parse(content: &str) -> HashMap<String, String> {
+    let mut variables = HashMap::new();
+
+    let re = Regex::new(r#"(?m)^(?P<key>\w+)=["']?(?P<value>[^"'\n#]+)["']?"#).unwrap();
+
+    for caps in re.captures_iter(content) {
+        let key = caps.name("key").unwrap().as_str().to_string();
+        let value = caps.name("value").unwrap().as_str().trim().to_string();
+
+        variables.insert(key, value);
+    }
+
+    variables
+}
+
 fn make(pkgbuild_path: &Path) {
     let content = fs::read_to_string(pkgbuild_path).unwrap();
-    println!("{}", content);
+
+    let vars = parse(&content);
+    let pkgname = vars.get("pkgname").map(|s| s.as_str()).unwrap_or("null");
+    let pkgver = vars.get("pkgver").map(|s| s.as_str()).unwrap_or("1.0.0");
+
+    println!("{pkgname}");
 }
